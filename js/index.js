5bc4193ac1e6c5343337843a38ac0ec7a938a496
Gabriel Oliveira <gabriel0211oliveira15@gmail.com>
2022-07-15 14:30:36 -0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+import { Router } from './routes.js';
+
+const router = new Router();
+router.add('/', '/pages/home.html');
+router.add('/universe', '/pages/universe.html');
+router.add('/explorer', '/pages/explorer.html');
+
+router.handle();
+
+window.onpopstate = () => router.handle();
+window.route = router.route();
+
+
